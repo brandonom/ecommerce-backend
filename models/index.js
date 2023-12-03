a36fff6d88afcb4a1a3e9d05cb7c28d7da51759a
brandonom <brandonomyers1@gmail.com>
2023-12-03 17:06:46 -0500

    fixed model associations

--- models/index.js
+++ models/index.js
@@ -12,6 +12,26 @@ const ProductTag = require('./ProductTag');
 
 // Tags belongToMany Products (through ProductTag)
 
+Product.belongsTo(Category,{
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+  
+});
+Category.hasMany(Product,{
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
+Product.belongsToMany(Tag,{
+  through: ProductTag,
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE'
+});
+Tag.belongsToMany(Product,{
+  through: ProductTag,
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE'
+});
+
 module.exports = {
   Product,
   Category,
